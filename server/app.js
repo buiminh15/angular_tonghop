4704976d0007c888310131bc36a05cd423c2e54c
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2020-03-15 19:47:11 +0700

    done CRUD

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+// ROUTERS
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
