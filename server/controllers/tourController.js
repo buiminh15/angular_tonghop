9fd2a6360facd5a69cc473b05244fbeebe7f486f
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2020-03-21 12:04:50 +0700

    done document middleware

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -167,6 +167,7 @@ exports.createTour = async (req, res) => {
       }
     });
   } catch (error) {
+    console.log(error)
     res.status(400).json({
       status: 'fail',
       message: 'Invalid data sent!'
--- server/models/tourModel.js
+++ server/models/tourModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const slugify = require('slugify');
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -8,6 +8,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: String,
     description: {
       type: String,
       required: [true, 'A tour must have a description'],
@@ -75,6 +76,22 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7
 })
+
+// DOCUMENT MIDDLEWARE: runs before save() and create() 
+tourSchema.pre('save', function() {
+  this.slug = slugify(this.name, { lower: true})
+  next()
+})
+
+// tourSchema.pre('save', function(next) {
+//  console.log('will save doc....')
+//   next();
+// });
+
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc)
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
