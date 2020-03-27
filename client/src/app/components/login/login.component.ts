888b966663033ef5b21b825a70d11c8ef9d36151
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2020-03-27 13:41:17 +0700

    auth-guard

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/shared/users.service';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { convertToJSON } from '../../shared/utils'
 @Component({
   selector: 'app-login',
@@ -11,7 +11,7 @@ import { convertToJSON } from '../../shared/utils'
 export class LoginComponent implements OnInit {
   email: string;
   password: string;
-  constructor(private service: UsersService, private router: Router) { }
+  constructor(private service: UsersService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
@@ -23,8 +23,11 @@ export class LoginComponent implements OnInit {
     }
     if (form.valid) {
       this.service.login(user).subscribe(res => {
-        alert('Done')
-        this.router.navigate(['/tours']);
+        if (res) {
+          alert('Done')
+          const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl')
+          this.router.navigate([returnUrl || '/']);
+        }
       },
         err => {
           alert(err.error.message);
--- client/src/app/shared/auth-guard.service.ts
+++ client/src/app/shared/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { UsersService } from './users.service';
 
 @Injectable({
@@ -9,11 +9,11 @@ export class AuthGuard implements CanActivate {
 
   constructor(private router: Router, private userService: UsersService) { }
 
-  canActivate() {
+  canActivate(route, state: RouterStateSnapshot) {
     if (this.userService.isLoggedIn()) {
       return true;
     }
-    this.router.navigate(['/login']);
+    this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
     return false;
   }
 }
