d97d9ab0933e49b94e938ac28867cb3d97bb1be3
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2020-05-06 18:36:57 +0700

    directive done

--- client/src/app/recipes/recipe-list/recipe-list.component.ts
+++ client/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 @Component({
@@ -7,13 +7,17 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a simple test', 'https://rasamalaysia.com/wp-content/uploads/2019/01/baked-chicken-breasts2.jpg'),
-    new Recipe('A Test Recipe', 'This is a simple test', 'https://rasamalaysia.com/wp-content/uploads/2019/01/baked-chicken-breasts2.jpg')
+    new Recipe('Another Test Recipe', 'This is a simple test', 'https://rasamalaysia.com/wp-content/uploads/2019/01/baked-chicken-breasts2.jpg')
   ]
   constructor() { }
 
   ngOnInit() {
   }
-
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
+  }
 }
