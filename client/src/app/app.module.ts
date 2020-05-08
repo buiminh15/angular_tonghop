794edef613b624de3dbf7d1523a3df7fa546c95d
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2020-05-08 09:50:24 +0700

    132 doing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -11,6 +12,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
+
 
 @NgModule({
   declarations: [
--- client/src/app/recipes/recipe-list/recipe.service.ts
+++ client/src/app/recipes/recipe-list/recipe.service.ts
@@ -1,19 +1,39 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a simple test', 'https://rasamalaysia.com/wp-content/uploads/2019/01/baked-chicken-breasts2.jpg'),
-    new Recipe('Another Test Recipe', 'This is a simple test', 'https://rasamalaysia.com/wp-content/uploads/2019/01/baked-chicken-breasts2.jpg')
+    new Recipe(
+      'Tasty Schitzel',
+      'A super-tasty Schitzal - just awesome!',
+      'https://rasamalaysia.com/wp-content/uploads/2019/01/baked-chicken-breasts2.jpg',
+      [
+        new Ingredient('Meat',1),
+        new Ingredient('French Fries', 20)
+      ]
+      ),
+      new Recipe(
+        'Big Fat Burger',
+        'What else need to say?',
+        'https://rasamalaysia.com/wp-content/uploads/2019/01/baked-chicken-breasts2.jpg',
+        [
+          new Ingredient('Buns',2),
+          new Ingredient('Meat', 1)
+        ]
+        ),
   ]
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice()
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients)
   }
 }
