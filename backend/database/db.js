f07dac2c45a0c8bda46e6ebf63223b500f6825f4
グエン ティエンマイン <dev10157@blcloudprj101.sakura.ne.jp>
2020-05-20 09:23:02 +0700

    start

---
+++ backend/database/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  db: 'mongodb+srv://minhbb:1234@cluster0-d9dj2.mongodb.net/meanfileupload?retryWrites=true&w=majority'
+}
+
+//mongodb+srv://minhbb:<password>@cluster0-d9dj2.mongodb.net/test?retryWrites=true&w=majority
+//  db: 'mongodb://localhost:27017/meanfileupload'
